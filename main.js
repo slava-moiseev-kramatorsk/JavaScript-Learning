e9ef6a3c3a2a959c191019248d15b430134c5130
slava-moiseev-kramatorsk <qualityengineerinkom@gmail.com>
2021-12-09 16:50:03 +0200

    input

--- main.js
+++ main.js
@@ -1,27 +1,43 @@
+const rates = {};
+const input = document.getElementById('inp');
+const output = document.getElementById('output');
+const select = document.getElementById('selector');
+
+getInfo();
 async function getInfo (){
 const response = await  fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
 const info = await response.json();
-const result = await info;
-    console.log(info)                       //// это дерьмище - массив
-    console.log(info[1].rate);              // работает через массив.
-    const obj = Object.assign({}, result);
-    // console.log(obj);                  //теперь это объект и невозможно получить значение,сука.
-    function mainUah(){
-        let var1 = document.getElementById('chf1');
-        let var2 = document.getElementById('usd1');
-        let var3 = document.getElementById('eur1');
-        var1.innerHTML = info[23].rate;
-        var2.innerHTML = info[26].rate;
-        var3.innerHTML = info[32].rate;
-        console.log(var1);
+const result = await info;                          //// это - массив
+
+    const obj = Object.assign({}, result);          //теперь это объект и невозможно получить значение,сука.
+
+    rates.usd = info[26].rate;
+    rates.eur = info[32].rate;
+    rates.chf = info[23].rate;
+    console.log(rates);
+    showActual();
     }
-    mainUah();
-    // onchange()
-    let currencyIn = document.getElementById('list').children[0].value;
-    console.log(currencyIn);
-
-
+function showActual(){
+    let var1 = document.getElementById('chf1');
+    let var2 = document.getElementById('usd1');
+    let var3 = document.getElementById('eur1');
+    var1.innerHTML = rates.chf;
+    var2.innerHTML = rates.usd;
+    var3.innerHTML = rates.eur;
 }
 
 
-getInfo();
+input.onchange = function (){
+    console.log('rabotaet');
+}
+
+
+
+    // let currencyIn = document.getElementById('list1').children[1].value;      // содержание выпдающего элемента по индексу
+    // console.log(currencyIn);
+
+
+
+
+
+
